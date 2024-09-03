63bc46362c0d58c58c007f4a01f59966baeb16c0
Erik Rosenlund <erik.rosenlund.work@gmail.com>
2024-09-03 11:34:38 +0200

    First graph

--- ssp_test/src/main.rs
+++ ssp_test/src/main.rs
@@ -1,10 +1,13 @@
 
 #![allow(unused_imports)]
 
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufReader, Write};
 
-// use petgraph::Graph;
+use petgraph::data::Build;
+use petgraph::dot::{Config, Dot};
+use petgraph::Graph;
 
 
 // use ssp::SSP;
@@ -12,35 +15,51 @@ use ssp_schema::ssp::ssd::SystemStructureDescription;
 
 
 fn main() -> std::io::Result<()> {
-    println!("Hello, world!");
+    log::trace!("Main!");
 
-    let ssp = SystemStructureDescription::new_from_file("ssp_test/reference_ssd/SystemStructure.ssd").expect("Failed to parse ssd");
+    log::trace!("Parsing ssd");
+    let ssd = SystemStructureDescription::new_from_file("ssp_test/reference_ssd/SystemStructure.ssd").expect("Failed to parse ssd");
 
-
-
+    log::trace!("Storing internal repressentation");
     let mut output_file = File::create("ssp_test/reference_ssd/parsed_xml")?;
-    write!(output_file, "{:#?}", ssp)?;
+    write!(output_file, "{:#?}", ssd)?;
 
+    log::trace!("Creating graph");
+    let mut graph = Graph::<&str, &str>::new();
     
-    // let mut deps = Graph::<&str, &str>::new();
+    log::trace!("Creating nodes");
+    let components  = ssd.System.Elements.unwrap().Components;
 
-    // let components  = ssd.System.Elements.unwrap().Components;
+    let mut comp_id = HashMap::new();
 
-    // for component in components.iter()
-    // {
-    //     println!("{:#?}", component);
-    // }
+    for component in components.iter()
+    {
+        let name = component.name.as_deref().expect("No name attribute found");
+        // .unwrap();
+        let id = graph.add_node(name);
 
-    // return Ok(());
+        comp_id.insert(name, id);
+
+        println!("{:#?}", name);
+    }
+
+    log::trace!("Creating edges");
+
+    let conections = ssd.System.Connections.unwrap().list;
+    for connection in conections.iter()
+    {
+        let start_node = connection.startElement.as_deref().expect("No start elelent existing");
+        let end_node = connection.endElement.as_deref().expect("No end elelent existing");
+        
+        println!("{:#?}", start_node);
+        println!("{:#?}", end_node);
+
+        graph.add_edge(comp_id[start_node], comp_id[end_node], "");
+
+    }
     
-    // let conections = ssd.System.Connections.unwrap().list;
-    // for connection in conections.iter()
-    // {
-    //     println!("{:#?}", connection);
-
-    // }
-
-
-
+    println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
+    
+    
     return Ok(());
 }
